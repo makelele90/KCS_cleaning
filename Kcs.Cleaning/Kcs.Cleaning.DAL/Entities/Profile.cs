4c5086033a34ba40c02b960439494a211561c489
delmensah <delmensah23@gmail.com>
2015-03-13 21:41:54 +0000

    property added in profile class and user class

--- Kcs.Cleaning/Kcs.Cleaning.DAL/Entities/Profile.cs
+++ Kcs.Cleaning/Kcs.Cleaning.DAL/Entities/Profile.cs
@@ -4,6 +4,9 @@ namespace Kcs.Cleaning.DAL.Entities
 
     public class Profile
     {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public User User { get; set; }
     }
 
 }
--- Kcs.Cleaning/Kcs.Cleaning.DAL/Entities/User.cs
+++ Kcs.Cleaning/Kcs.Cleaning.DAL/Entities/User.cs
@@ -4,6 +4,16 @@ namespace Kcs.Cleaning.DAL.Entities
 
     public class User
     {
+        public virtual int UserId { get; set; }
+        public virtual string Username { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string PasswordSalt { get; set; }
+        public virtual DateTime CreatedOn { get; set; }
+        public virtual DateTime LastUpadted { get; set; }
+        public virtual DateTime LastLogin { get; set; }
+        public virtual bool IsActive { get; set; }
+        public virtual bool IsPaswordLocked { get; set; }
+        public Profile Profile { get; set; }
     }
 
 }
