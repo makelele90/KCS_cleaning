63575c4236c4762b81ffa5c1dfa40e7d887481af
delmensah <delmensah23@gmail.com>
2015-03-17 00:10:40 +0000

    profile id added to profile class

--- Kcs.Cleaning/Kcs.Cleaning.DAL/Entities/Profile.cs
+++ Kcs.Cleaning/Kcs.Cleaning.DAL/Entities/Profile.cs
@@ -5,6 +5,7 @@ namespace Kcs.Cleaning.DAL.Entities
     public class Profile
     {
         public int UserId { get; set; }
+        public int profileId { get; set; }
         public string Email { get; set; }
         public User User { get; set; }
     }
